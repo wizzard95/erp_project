d77e94a5142fcad3511c3cf48bf6f113b9988619
wizzard95 <damian110995a@gmail.com>
2026-01-20 21:09:52 -0300

    config(dashboard.html): configurar las tarjetas de los modulos junto con sus permisos correspondientes

--- core/views.py
+++ core/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render 
+from django.contrib.auth.decorators import login_required
+from users.models import UserRole
 # Create your views here.
+
+# ? vamos a calcular que permisos tiene el usuario
+
+@login_required
+def dashboard_view(request):
+
+    user_roles = UserRole.objects.filter(user_id=request.user)
+
+    permissions = {
+        'customers': 0,
+        'suppliers': 0,
+        'materials': 0,
+        'purchases': 0,
+        'sales': 0,
+        'inventory': 0,
+        'accounting': 0,
+        'reporting': 0,
+    }
+
+    for user_role in user_roles:
+        role = user_role.role
+        for module in permissions.keys():
+            current_permission = getattr(role, module)
+            if current_permission > permissions[module]:
+                permissions[module] = current_permission
+    
+    context = {
+        'user': request.user,
+        'permissions': permissions,
+        'roles': [ur.role.role_name for ur in user_roles],
+    }
+
+    return render(request, 'core/dashboard.html', context)
