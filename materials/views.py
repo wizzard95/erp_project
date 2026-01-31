047185f9cda51af3aa64121ebf574bb64aa5371b
wizzard95 <damian110995a@gmail.com>
2026-01-31 19:17:48 -0300

    config(materials/views.py): crear el renderizado para listar materiales

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Material
+from django.core.paginator import Paginator
+from django.db import models
+from users.models import UserRole
 
 # Create your views here.
+
+# * listar materiales
+@login_required
+def material_list(request):
+
+    max_permission = UserRole.objects.filter(user_id=request.user).aaggregate(max_permission=models.Max('role_materials'))['max_permission'] or 0
+
+    if max_permission == 0:
+        return redirect('dashboard')
+    
+    materials_list = Material.objects.all()
+
+    id_material = request.GET.get('id_material')
+    name = request.GET.get('name')
+    material_type = request.GET.get('material_type')
+    status = request.GET.get('status')
+
+    
+    if id_material:
+        materials_list = materials_list.filter(id_material__icontains=id_material)
+    
+    if name:
+        materials_list = materials_list.filter(name__icontains=name)
+    
+    if material_type:
+        materials_list = materials_list.filter(material_type__icontains=material_type)
+
+    if material_type:
+        materials_list = materials_list.filter(material_type__icontains=material_type)
+
+    if status is not None and status !='':
+        materials_list = materials_list.filter(status=status)
+
+    
+    paginator = Paginator(material_list,10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'materials/materials_list.html', {'page_obj': page_obj})
