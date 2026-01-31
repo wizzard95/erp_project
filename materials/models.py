4c99d16923a803a33972a9c03372e4dcbf155857
wizzard95 <damian110995a@gmail.com>
2026-01-31 18:33:15 -0300

    config(materials/models.py): creacion de la clase material dentro del modelo

---
+++ materials/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Material(models.Model):
+
+    id_material = models.CharField(max_length=50, null= True, blank=True,verbose_name="Material_ID")
+    name = models.CharField(max_length=100, verbose_name="Name")
+    description = models.TextField(max_length=250, blank=True, verbose_name="Description")
+    unit = models.CharField(max_length=50, verbose_name="Unit measure")
+    material_type = models.CharField(max_length=50, verbose_name="Material type")
+    status = models.CharField(max_length=50,verbose_name="Status")
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, null= True)
+
+
+    class Meta:
+        verbose_name = "Material"
+        verbose_name_plural = "Materials"
+
+    
+    def __str__(self):
+        return self.name
