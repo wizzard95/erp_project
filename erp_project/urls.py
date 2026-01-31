c2c067a76b443303880f5ea2f34d9edb26fcdd4a
wizzard95 <damian110995a@gmail.com>
2026-01-31 19:45:23 -0300

    config(erp_project/urls.py): añadir la url de materials para poder llamarla desde donde sea

--- erp_project/urls.py
+++ erp_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('core.urls')),
+    path('materials/', include('materials.urls')),
 ]
